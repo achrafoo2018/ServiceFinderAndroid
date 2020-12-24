09b26678ce3059f8c6293f0939395f1db6222b2d
AllelaAzaiez <allala.azaiz@gmail.com>
2020-12-24 21:44:09 +0100

    add post done

--- app/src/main/java/com/example/servicefinder/HomeActivity.java
+++ app/src/main/java/com/example/servicefinder/HomeActivity.java
@@ -1,14 +1,44 @@
 package com.example.servicefinder;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class HomeActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+    private static final int GALLERY_ADD_POST = 2;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+    }
+
+    private void init() {
+        navigationView = findViewById(R.id.bottom_nav);
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_PICK);
+            i.setType("image/*");
+            startActivityForResult(i, GALLERY_ADD_POST);
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_ADD_POST && resultCode==RESULT_OK){
+            Uri imgUri = data.getData();
+            Intent i = new Intent(HomeActivity.this,AddPostActivity.class);
+            i.setData(imgUri);
+            startActivity(i);
+        }
     }
 }
