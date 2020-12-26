6519d3d1e080ef6fa16d477cfef511de533b1059
= <=>
2020-12-27 00:52:54 +0100

    edit account works. More things to add

---
+++ app/src/main/java/com/example/servicefinder/EditUserInfoActivity.java
@@ -0,0 +1,183 @@
+package com.example.servicefinder;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class EditUserInfoActivity extends AppCompatActivity {
+
+    private TextInputLayout layoutName,layoutLastname,layoutEmail;
+    private TextInputEditText txtName,txtLastname,txtEmail;
+    private TextView txtSelectPhoto;
+    private Button btnSave;
+    private CircleImageView circleImageView;
+    private static final int GALLERY_CHANGE_PROFILE = 5;
+    private Bitmap bitmap = null;
+    private SharedPreferences userPref;
+    private ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_info);
+        init();
+    }
+
+    private void init() {
+        dialog = new ProgressDialog(this);
+        dialog.setCancelable(false);
+        userPref = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+        layoutLastname = findViewById(R.id.txtEditLayoutLastNameUserInfo);
+        layoutName = findViewById(R.id.txtEditLayoutFirstNameUserInfo);
+        layoutEmail = findViewById(R.id.txtEditLayoutEmailUserInfo);
+        txtName = findViewById(R.id.txtEditFirstNameUserInfo);
+        txtLastname = findViewById(R.id.txtEditLastNameUserInfo);
+        txtEmail = findViewById(R.id.txtEditEmailUserInfo);
+        txtSelectPhoto = findViewById(R.id.txtEditSelectPhoto);
+        btnSave = findViewById(R.id.btnEditSave);
+        circleImageView = findViewById(R.id.imgEditUserInfo);
+
+        Picasso.get().load(getIntent().getStringExtra("imgUrl")).into(circleImageView);
+        txtName.setText(userPref.getString("first_name",""));
+        txtLastname.setText(userPref.getString("last_name",""));
+        txtEmail.setText(userPref.getString("email",""));
+
+        txtSelectPhoto.setOnClickListener(v->{
+            Intent i = new Intent(Intent.ACTION_PICK);
+            i.setType("image/*");
+            startActivityForResult(i,GALLERY_CHANGE_PROFILE);
+        });
+
+        btnSave.setOnClickListener(v->{
+            if (validate()){
+                updateProfile();
+            }
+        });
+    }
+
+
+    private void updateProfile(){
+        dialog.setMessage("Updating");
+        dialog.show();
+        StringRequest request = new StringRequest(Request.Method.POST,Constant.SAVE_USER_INFO,res->{
+
+            try {
+                JSONObject object = new JSONObject(res);
+                if (object.getBoolean("success")){
+                    SharedPreferences.Editor editor = userPref.edit();
+                    editor.putString("first_name",txtName.getText().toString().trim());
+                    editor.putString("last_name",txtLastname.getText().toString().trim());
+                    editor.putString("email",txtEmail.getText().toString().trim());
+                    editor.apply();
+                    Toast.makeText(this, "Profile Updated!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            dialog.dismiss();
+        },err->{
+            err.printStackTrace();
+            dialog.dismiss();
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                String token = userPref.getString("token","");
+                HashMap<String,String> map = new HashMap<>();
+                map.put("Authorization","Bearer "+token);
+                return map;
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+                map.put("first_name",txtName.getText().toString().trim());
+                map.put("last_name",txtLastname.getText().toString().trim());
+                map.put("email",txtEmail.getText().toString().trim());
+                map.put("profile_picture",bitmapToString(bitmap));
+                return map;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(EditUserInfoActivity.this);
+        queue.add(request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==GALLERY_CHANGE_PROFILE && resultCode==RESULT_OK){
+            Uri uri = data.getData();
+
+            circleImageView.setImageURI(uri);
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean validate(){
+        if (txtName.getText().toString().isEmpty()){
+            layoutName.setErrorEnabled(true);
+            layoutName.setError("First Name is required!");
+            return false;
+        }
+        if (txtLastname.getText().toString().isEmpty()){
+            layoutLastname.setErrorEnabled(true);
+            layoutLastname.setError("Last Name is required!");
+            return false;
+        }
+        if (txtEmail.getText().toString().isEmpty()){
+            layoutEmail.setErrorEnabled(true);
+            layoutEmail.setError("Email is required!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private String bitmapToString(Bitmap bitmap) {
+        if (bitmap!=null){
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+            byte [] array = byteArrayOutputStream.toByteArray();
+            return Base64.encodeToString(array,Base64.DEFAULT);
+        }
+
+        return "";
+    }
+}
