d80dcd1495553bd30664a747fd07c07f7ca662f1
AllelaAzaiez <allala.azaiz@gmail.com>
2021-01-05 21:32:06 +0100

    search complete2

--- app/src/main/java/com/example/servicefinder/Adapters/PostsAdapter.java
+++ app/src/main/java/com/example/servicefinder/Adapters/PostsAdapter.java
@@ -9,6 +9,7 @@ import android.widget.Filter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
